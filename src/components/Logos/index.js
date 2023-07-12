925e0949620f978aa76ecd6d8d66e419f37f0824
Cleison Carlos <cleisonspider@yahoo.com.br>
2023-07-12 14:07:23 -0300

    areas de clientes adicionado ao site

--- src/components/Logos/index.js
+++ src/components/Logos/index.js
@@ -4,10 +4,19 @@ import React from "react";
 import Slider from "react-slick";
 
 import ananindeua from "../../images/logos/ananindeua.png";
+import ananews from "../../images/logos/ananews.png";
 import alepa from "../../images/logos/alepa.png";
 import benevides from "../../images/logos/benevides.png";
-import amapa from "../../images/logos/amapa.png";
-import para from "../../images/logos/para.png";
+import agenciabenevides from "../../images/logos/agenciabenevides.png";
+import governoamapa from "../../images/logos/governoamapa.png";
+import governopara from "../../images/logos/governopara.png";
+import fundacaocultural from "../../images/logos/fundacaocultural.png";
+import drnumeros from "../../images/logos/drnumeros.png";
+import leialdirblanc from "../../images/logos/leialdirblanc.png";
+import sbseg from "../../images/logos/sbseg.png";
+import secult from "../../images/logos/secult.png";
+import prefeiturasaoluiz from "../../images/logos/prefeiturasaoluiz.png";
+import prefeituracastanhal from "../../images/logos/prefeituracastanhal.png";
 
 export default function Logos() {
   const settings = {
@@ -20,8 +29,31 @@ export default function Logos() {
   return (
     <div className="my-10 mx-10 align-middle">
       <Slider {...settings}>
+
+      <div>
+          <Image src={prefeituracastanhal} alt="nodejs" />
+        </div>
+
+      <div>
+          <Image src={prefeiturasaoluiz} alt="nodejs" />
+        </div>
+
+
+      <div>
+          <Image src={sbseg} alt="nodejs" />
+        </div>
+
+
+
         <div>
           <Image src={ananindeua} alt="nodejs" />
+        </div>
+        <div>
+          <Image src={secult} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={ananews} alt="nodejs" />
         </div>
 
         <div>
@@ -33,7 +65,30 @@ export default function Logos() {
         </div>
 
         <div>
-          <Image src={amapa} alt="nodejs" />
+          <Image src={agenciabenevides} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={governoamapa} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={governopara} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={fundacaocultural} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={benevides} alt="nodejs" />
+        </div>
+        <div>
+          <Image src={drnumeros} alt="nodejs" />
+        </div>
+
+        <div>
+          <Image src={leialdirblanc} alt="nodejs" />
         </div>
       </Slider>
     </div>
